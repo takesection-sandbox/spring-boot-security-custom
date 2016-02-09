40ca434ad5bd3fa4a8ed413e4ad1d67c42155247
s_edward <takesection@gmail.com>
2016-02-09 21:57:40 +0900

    Filterの設定を修正

--- src/main/java/jp/pigumer/app/WebSecurityConfig.java
+++ src/main/java/jp/pigumer/app/WebSecurityConfig.java
@@ -37,7 +37,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests().antMatchers("/").permitAll().anyRequest().authenticated();
-        http.regexMatcher("^/secure.*").addFilterAfter(exampleFilter(), BasicAuthenticationFilter.class);
+        http.addFilterAfter(exampleFilter(), BasicAuthenticationFilter.class);
         http.csrf().disable();
     }
     
